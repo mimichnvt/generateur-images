3d62171224793e3926d7797a3cec77e7b9fd3264
Emilie <emilie.chnvt@gmail.com>
2024-01-10 12:13:14 +0100

    add variables

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-var button = document.getElementById('new-img');
-var image = document.querySelector('.container')
+
+
+// définitions des variables
+var button = document.querySelector('button');
+var image = document.querySelector('.container');
+
+button.onclick = function (){
+    // le lien de l'API
+    let url = ''
+
+}
